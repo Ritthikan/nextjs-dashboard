ada9c79356530f5b0464ad74d99b53ee67e3f2e7
LazyJune <nepjunex@gmail.com>
2024-04-17 11:14:35 +0100

    modified:   app/ui/invoices/[id]/edit/page.tsx

--- app/ui/invoices/[id]/edit/page.tsx
+++ app/ui/invoices/[id]/edit/page.tsx
@@ -8,7 +8,7 @@ export default async function Page({ params }: { params: { id: string } }) {
     const [invoice, customers] = await Promise.all([
         fetchInvoiceById(id),
         fetchCustomers(),
-    ]);
+    ])
     if (!invoice) {
       notFound();
     }
